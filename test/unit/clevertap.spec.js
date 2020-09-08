b61ccd0209976257623c6984640e5752da90300e
Victor Fernandes <4434814+victorfdes@users.noreply.github.com>
2020-09-08 11:18:48 +0530

    add tests

--- test/unit/clevertap.spec.js
+++ test/unit/clevertap.spec.js
@@ -1,4 +1,3 @@
-import clevertap from '../../clevertap'
 import Clevertap from '../../src/clevertap'
 
 test('Clevertap has event', () => {
@@ -11,12 +10,59 @@ test('Clevertap has profile', () => {
   expect(ct.profile).toBeDefined()
 })
 
+test('Clevertap has account', () => {
+  const ct = new Clevertap()
+  expect(ct.account).toBeDefined()
+})
+
+test('Clevertap has onUserLogin', () => {
+  const ct = new Clevertap()
+  expect(ct.onUserLogin).toBeDefined()
+})
+
+test('Clevertap has notifications', () => {
+  const ct = new Clevertap()
+  expect(ct.notifications).toBeDefined()
+})
+
+test('Clevertap has privacy', () => {
+  const ct = new Clevertap()
+  expect(ct.privacy).toBeDefined()
+})
+
 test('Event can be pushed', () => {
   const ct = new Clevertap()
   expect(ct.event.push).toBeInstanceOf(Function)
 })
 
-test('Clevertap has event key', () => {
+test('Profile can be pushed', () => {
   const ct = new Clevertap()
   expect(ct.profile.push).toBeInstanceOf(Function)
+})
+
+test('Account can be pushed', () => {
+  const ct = new Clevertap()
+  expect(ct.account.push).toBeInstanceOf(Function)
+})
+
+test('onUserLogin can be pushed', () => {
+  const ct = new Clevertap()
+  expect(ct.onUserLogin.push).toBeInstanceOf(Function)
+})
+
+test('Notifications can be pushed', () => {
+  const ct = new Clevertap()
+  expect(ct.notifications.push).toBeInstanceOf(Function)
+})
+
+test('Privacy can be pushed', () => {
+  const ct = new Clevertap()
+  expect(ct.privacy.push).toBeInstanceOf(Function)
+})
+
+test('Clevertap accepts account ID as per Web SDK docs', () => {
+  global.clevertap = {event:[], profile:[], account:[], onUserLogin:[], notifications:[], privacy:[]} // Will be added by customer during installation
+  global.clevertap.account.push({"id": "44Z-R45-995Z"})
+  const ct = new Clevertap(global.clevertap)
+  expect(ct['account'][0]['id']).toEqual('44Z-R45-995Z')
 })
