da27af3c31cf7ac5a1983c300e57f6908e1d4cc1
Victor Fernandes <4434814+victorfdes@users.noreply.github.com>
2020-09-09 10:45:14 +0530

    update clevertap constructor

--- src/clevertap.js
+++ src/clevertap.js
@@ -2,7 +2,15 @@ import options from './options'
 export default class CleverTap {
   constructor (clevertap = {}) {
     this.options = {...options}
-    this.event = clevertap.event || []
-    this.profile = clevertap.profile || []
+    this.event = Array.isArray(clevertap.event) ? clevertap.event : []
+    this.profile = Array.isArray(clevertap.profile) ? clevertap.profile : []
+    this.account = Array.isArray(clevertap.account) ? clevertap.account : []
+    this.onUserLogin = Array.isArray(clevertap.onUserLogin) ? clevertap.onUserLogin : []
+    this.notifications = Array.isArray(clevertap.notifications) ? clevertap.notifications : []
+    this.privacy = Array.isArray(clevertap.privacy) ? clevertap.privacy : []
+  }
+  
+  init () {
+    // Initialize the plugin
   }
 }
