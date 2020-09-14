faec179ef1ae925d3bb8a3645f3a938bd708ef75
Victor Fernandes <4434814+victorfdes@users.noreply.github.com>
2020-09-14 16:45:04 +0530

    add babel/plugin-proposal-class-properties

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-    presets: [
-      [
-        '@babel/preset-env',
-        {
-          targets: {
-            node: 'current',
-          },
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
         },
-      ],
+      },
     ],
-  };
+  ],
+  plugins: [
+    ["@babel/plugin-proposal-class-properties", { "loose": true }]
+  ]
+}
