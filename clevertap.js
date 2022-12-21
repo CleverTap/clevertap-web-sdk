0fcf08cf2bd67a8f1cadea24df578d715bc6f5cb
Ayush Sinha <ayush.sinha@clevertap.com>
2022-12-21 15:03:18 +0530

    fixes divHeight for carousel

--- clevertap.js
+++ clevertap.js
@@ -3508,7 +3508,7 @@
     }, {
       key: "getStyles",
       value: function getStyles() {
-        return "\n      <style>\n      .carousel {\n        position: relative;\n      }\n\n      .carousel__item {\n        background-color: grey;\n        display: none;\n        background-repeat: no-repeat;\n        background-size: cover;\n      }\n\n      .carousel__item img {\n        height: ".concat(this.divHeight ? this.divHeight : 'auto', ";\n        width: 100%;\n        transition: 2s;\n      }\n\n      .carousel__item--selected {\n        display: block;\n      }\n      ").concat(this.display.navBtnsCss, "\n      ").concat(this.display.navArrowsCss, "\n      </style>\n  ");
+        return "\n      <style>\n      .carousel {\n        position: relative;\n      }\n\n      .carousel__item {\n        background-color: grey;\n        display: none;\n        background-repeat: no-repeat;\n        background-size: cover;\n      }\n\n      .carousel__item img {\n        height: ".concat(this.target && this.target.display && this.target.display.divHeight ? this.target.display.divHeight : 'auto', ";\n        width: 100%;\n        transition: 2s;\n      }\n\n      .carousel__item--selected {\n        display: block;\n      }\n      ").concat(this.display.navBtnsCss, "\n      ").concat(this.display.navArrowsCss, "\n      </style>\n  ");
       }
     }, {
       key: "updateSelectedItem",
@@ -3850,7 +3850,6 @@
       var divId = targetingMsgJson.display.divId;
       var carousel = document.createElement('ct-web-personalisation-carousel');
       carousel.target = targetingMsgJson;
-      carousel.divHeight = targetingMsgJson.display.divHeight;
       var container = document.getElementById(divId);
       container.innerHTML = '';
       container.appendChild(carousel);
