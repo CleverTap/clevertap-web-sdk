b4f9ba916f0a60dc7cc905a2b6dc2bbf089e49b4
KambleSonam <sonam@clevertap.com>
2024-07-10 15:54:40 +0530

    Updated the overlay and styles path

--- src/modules/visualBuilder/builder_constants.js
+++ src/modules/visualBuilder/builder_constants.js
@@ -1,5 +1,2 @@
-// export const OVERLAY_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/overlay.js'
-// export const CSS_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/style.css'
-
-export const OVERLAY_PATH = 'https://raw.githubusercontent.com/ThisIsRaghavGupta/shiny-pancake/main/overlay.js'
-export const CSS_PATH = 'https://raw.githubusercontent.com/ThisIsRaghavGupta/shiny-pancake/main/style.css'
+export const OVERLAY_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/staging/overlay.js'
+export const CSS_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/staging/style.css'
