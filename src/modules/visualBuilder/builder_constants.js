902a7b64ddffa28ee5b2abb7d70aab980a86568f
KambleSonam <sonam@clevertap.com>
2024-07-11 13:19:23 +0530

    Overlay and CSS path update

--- src/modules/visualBuilder/builder_constants.js
+++ src/modules/visualBuilder/builder_constants.js
@@ -1,2 +1,2 @@
-export const OVERLAY_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/staging/overlay.js'
-export const CSS_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/staging/style.css'
+export const OVERLAY_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay-staging/overlay.js'
+export const CSS_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay-staging/style.css'
