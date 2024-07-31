dfb5daeff5a444a120d0162f80f7a676eaed78eb
KambleSonam <98796470+KambleSonam@users.noreply.github.com>
2024-07-31 13:40:04 +0530

    Web SDK Release v1.9.0 (#240)
    * Increased the timer to 300ms for adjustIframeHeight
    
    * Point and Click form handling (#237)
    
    * point and click form update logic and code refactor
    
    * Point and Click form handling update
    
    * Updated the function updateFormData
    
    * Update the form Update logic
    
    * Update the form handling for elementCss
    
    * Updated the overlay and styles path
    
    * Overlay and CSS path update
    
    * Updated the overlay and CSS path
    
    * Updated overlay and css path
    
    * Updated package.json and changelog
    
    ---------
    
    Co-authored-by: Yusuf Khan <154309421+kkyusuftk@users.noreply.github.com>

--- src/modules/visualBuilder/builder_constants.js
+++ src/modules/visualBuilder/builder_constants.js
@@ -1,2 +1,2 @@
-export const OVERLAY_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/overlay.js'
-export const CSS_PATH = 'https://d2r1yp2w7bby2u.cloudfront.net/js/lib-overlay/style.css'
+export const OVERLAY_PATH = 'https://web-native-display-campaign.clevertap.com/production/lib-overlay/overlay.js'
+export const CSS_PATH = 'https://web-native-display-campaign.clevertap.com/production/lib-overlay/style.css'
