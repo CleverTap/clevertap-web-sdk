5229f222a19f82413e88265d08c6d86ffac6993a
Kunal SIngh <49939683+singhkunal2050@users.noreply.github.com>
2025-03-06 15:45:57 +0530

    [WEB-3494]Added Support for Delivery Priority for Web Native Display (#357)

--- src/util/campaignRender/nativeDisplay.js
+++ src/util/campaignRender/nativeDisplay.js
@@ -1,3 +1,4 @@
+import { CUSTOM_EVENT_KEYS, CUSTOM_EVENTS_CAMPAIGN_SOURCES } from '../constants'
 import { CTWebPersonalisationBanner } from '../web-personalisation/banner'
 import { CTWebPersonalisationCarousel } from '../web-personalisation/carousel'
 
@@ -37,7 +38,12 @@ export const handleKVpairCampaign = (targetingMsgJson) => {
   if (targetingMsgJson.msgContent.kv != null) {
     inaObj.kv = targetingMsgJson.msgContent.kv
   }
-  const kvPairsEvent = new CustomEvent('CT_web_native_display', { detail: inaObj })
+  // combine all events from web native display under single event and add type
+  const kvPairsEvent = new CustomEvent(CUSTOM_EVENT_KEYS.WEB_NATIVE_DISPLAY, {
+    detail: {
+      campaignDetails: inaObj, campaignSource: CUSTOM_EVENTS_CAMPAIGN_SOURCES.KV_PAIR
+    }
+  })
   document.dispatchEvent(kvPairsEvent)
 }
 
@@ -95,6 +101,10 @@ export const handleJson = (targetingMsgJson) => {
   if (targetingMsgJson.display.json != null) {
     inaObj.json = json
   }
-  const jsonEvent = new CustomEvent('CT_web_native_display_json', { detail: inaObj })
+  const jsonEvent = new CustomEvent(CUSTOM_EVENT_KEYS.WEB_NATIVE_DISPLAY, {
+    detail: {
+      campaignDetails: inaObj, campaignSource: CUSTOM_EVENTS_CAMPAIGN_SOURCES.JSON
+    }
+  })
   document.dispatchEvent(jsonEvent)
 }
