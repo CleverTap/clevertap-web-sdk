311390e6f7a8dc53074564ab20d9cedfca019ebe
Kunal SIngh <kunal.singh@clevertap.com>
2025-06-09 09:42:52 +0530

    [WEB-3749] Version Bump

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,7 @@
 # Change Log
 All notable changes to this project will be documented in this file.
 
-## [1.16.0] 29th May, 2025
+## [1.15.4] 09th June, 2025 
 - Fix for Exit Intent Issue
 
 ## [1.15.3] 28nd May, 2025
--- clevertap.js
+++ clevertap.js
@@ -11623,7 +11623,7 @@
         case WVE_QUERY_PARAMS.SDK_CHECK:
           if (parentWindow) {
             logger$1.debug('SDK version check');
-            const sdkVersion = '1.16.0';
+            const sdkVersion = '1.15.4';
             parentWindow.postMessage({
               message: 'SDKVersion',
               accountId,
@@ -15299,7 +15299,7 @@
       let proto = document.location.protocol;
       proto = proto.replace(':', '');
       dataObject.af = { ...dataObject.af,
-        lib: 'web-sdk-v1.16.0',
+        lib: 'web-sdk-v1.15.4',
         protocol: proto,
         ...$ct.flutterVersion
       }; // app fields
@@ -17148,7 +17148,7 @@
     }
 
     getSDKVersion() {
-      return 'web-sdk-v1.16.0';
+      return 'web-sdk-v1.15.4';
     }
 
     defineVariable(name, defaultValue) {
