eb599558f6fab4a83b4643d608df2edfb06a1bc7
Kunal SIngh <kunal.singh@clevertap.com>
2025-06-26 19:27:22 +0530

    Rebasing with Current Changes

--- src/util/helpers.js
+++ src/util/helpers.js
@@ -1,3 +1,5 @@
+import { CUSTOM_CT_ID_PREFIX } from '../util/constants'
+
 export const isChrome = () => {
   const ua = navigator.userAgent
   return ua.includes('Chrome') || ua.includes('CriOS')
@@ -93,7 +95,6 @@ export const reconstructNestedObject = (payload) => {
 
   return result
 }
-
 
 /**
  * Validates and sanitizes a custom CleverTap ID based on platform rules.
