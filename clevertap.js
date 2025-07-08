d41a9d272dc2fc6fdd489d8388eb45efe33e67d6
AshwinSaxena01 <ashwin.saxena@clevertap.com>
2025-07-08 11:00:21 +0530

    updated version

--- clevertap.js
+++ clevertap.js
@@ -11657,7 +11657,7 @@
         case WVE_QUERY_PARAMS.SDK_CHECK:
           if (parentWindow) {
             logger$1.debug('SDK version check');
-            const sdkVersion = '1.17.1';
+            const sdkVersion = '1.17.0';
             parentWindow.postMessage({
               message: 'SDKVersion',
               accountId,
@@ -15532,7 +15532,7 @@
       let proto = document.location.protocol;
       proto = proto.replace(':', '');
       dataObject.af = { ...dataObject.af,
-        lib: 'web-sdk-v1.17.1',
+        lib: 'web-sdk-v1.17.0',
         protocol: proto,
         ...$ct.flutterVersion
       }; // app fields
@@ -17381,7 +17381,7 @@
     }
 
     getSDKVersion() {
-      return 'web-sdk-v1.17.1';
+      return 'web-sdk-v1.17.0';
     }
 
     defineVariable(name, defaultValue) {
