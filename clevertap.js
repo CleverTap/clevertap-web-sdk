a6a1c71652c94054974182045dc45fa332fecc1d
raghav.gupta <raghav.gupta1999@gmail.com>
2025-07-21 13:43:28 +0530

    Corrected image only click actions

--- clevertap.js
+++ clevertap.js
@@ -11657,7 +11657,7 @@
         case WVE_QUERY_PARAMS.SDK_CHECK:
           if (parentWindow) {
             logger$1.debug('SDK version check');
-            const sdkVersion = '1.17.1';
+            const sdkVersion = '1.17.2';
             parentWindow.postMessage({
               message: 'SDKVersion',
               accountId,
@@ -15542,7 +15542,7 @@
       let proto = document.location.protocol;
       proto = proto.replace(':', '');
       dataObject.af = { ...dataObject.af,
-        lib: 'web-sdk-v1.17.1',
+        lib: 'web-sdk-v1.17.2',
         protocol: proto,
         ...$ct.flutterVersion
       }; // app fields
@@ -17391,7 +17391,7 @@
     }
 
     getSDKVersion() {
-      return 'web-sdk-v1.17.1';
+      return 'web-sdk-v1.17.2';
     }
 
     defineVariable(name, defaultValue) {
