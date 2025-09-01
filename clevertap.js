2e57ffceada24ef68df585db14bb6b7cf83d2bbd
AshwinSaxena01 <ashwin.saxena@clevertap.com>
2025-09-01 17:39:09 +0530

    handled every case

--- clevertap.js
+++ clevertap.js
@@ -12620,7 +12620,11 @@
         this.container.style.setProperty('height', 'auto');
         this.container.style.setProperty('position', 'fixed');
         this.popup.style.setProperty('visibility', 'visible');
-        this.closeIcon.style.setProperty('visibility', 'visible');
+
+        if (this.closeIcon) {
+          this.closeIcon.style.setProperty('visibility', 'visible');
+        }
+
         document.getElementById('wzrkImageOnlyDiv').style.visibility = 'visible';
       };
     }
