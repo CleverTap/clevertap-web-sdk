9dcc085acc37032074230c73c5f409d37ba98ddc
KambleSonam <sonam@clevertap.com>
2026-08-14 13:55:27 +0530

    Added backup interval

--- src/clevertap.js
+++ src/clevertap.js
@@ -1006,7 +1006,12 @@ export default class CleverTap {
     initDiscardedEventsFromStorage()
     this.#processOldValues()
     this.pageChanged()
-    this.#request.processBackupEvents()
+    const backupInterval = setInterval(() => {
+      if (this.#device.gcookie) {
+        clearInterval(backupInterval)
+        this.#request.processBackupEvents()
+      }
+    }, 3000)
     if (this.#isSpa) {
       // listen to click on the document and check if URL has changed.
       document.addEventListener('click', this.#boundCheckPageChanged)
